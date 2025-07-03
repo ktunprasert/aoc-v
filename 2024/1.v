db8d37143e596503472aa1d6024bf6be8d934df5
ktunprasert <ktunprasert@outlook.com>
2025-07-04 00:20:18 +0100

    feat(2024.1): try going forward as module=main

--- 2024/1.v
+++ 2024/1.v
@@ -1,6 +1,24 @@
-module aoc2024_day1
+// module aoc2024_day1
+module main
+
+import os
 
 const input_file = '1.txt'
 const example_file = '1e.txt'
 
-fn parse_input() {}
+fn parse_input(filename string) ![]int {
+	content := os.read_file(filename)!
+
+	return content.fields().map(it.int())
+}
+
+fn main() {
+	dump(os.args)
+	mut filename := input_file
+	if os.args.len > 1 {
+		filename = os.args[1]
+	}
+
+	input := parse_input(filename)!
+	dump(input)
+}
