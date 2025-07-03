ce12067c1fcc1b0a79204406900845b900bbb0bb
ktunprasert <ktunprasert@outlook.com>
2025-07-04 00:28:22 +0100

    feat(2024.1): parsing ok, add example.txt

--- 2024/1.v
+++ 2024/1.v
@@ -6,14 +6,28 @@ import os
 const input_file = '1.txt'
 const example_file = '1e.txt'
 
-fn parse_input(filename string) ![]int {
+fn parse_input(filename string) ![][]int {
 	content := os.read_file(filename)!
 
-	return content.fields().map(it.int())
+	mut input := [][]int{len: 2, init: []int{}}
+	for i, n in content.fields().map(it.int()) {
+		if i & 1 == 1 {
+			input[1] << n
+			continue
+		}
+
+		input[0] << n
+	}
+
+	return input
+}
+
+fn part1(input []int) int {
+	return 0
 }
 
 fn main() {
-	dump(os.args)
+	// dump(os.args)
 	mut filename := input_file
 	if os.args.len > 1 {
 		filename = os.args[1]
