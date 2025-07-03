085f0bc6b454b4e1ec255b748f536c44fd65c5a0
ktunprasert <ktunprasert@outlook.com>
2025-07-04 00:39:54 +0100

    feat(2024.1): part 1 ok

--- 2024/1.v
+++ 2024/1.v
@@ -2,12 +2,14 @@
 module main
 
 import os
+import arrays
 
 const input_file = '1.txt'
 const example_file = '1e.txt'
 
 fn parse_input(filename string) ![][]int {
 	content := os.read_file(filename)!
+	// dump(content.limit(50) + '...')
 
 	mut input := [][]int{len: 2, init: []int{}}
 	for i, n in content.fields().map(it.int()) {
@@ -22,17 +24,26 @@ fn parse_input(filename string) ![][]int {
 	return input
 }
 
-fn part1(input []int) int {
-	return 0
+fn abs_diff[T](a T, b T) T {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
+
+fn part1(input [][]int) int {
+	sorted := input.map(it.sorted())
+	return arrays.sum(arrays.group[int](sorted[0], sorted[1]).map(abs_diff(it[0], it[1]))) or { -1 }
 }
 
 fn main() {
-	// dump(os.args)
 	mut filename := input_file
 	if os.args.len > 1 {
 		filename = os.args[1]
 	}
 
 	input := parse_input(filename)!
-	dump(input)
+	// dump(input)
+
+	println(part1(input))
 }
