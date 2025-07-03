c8d38fe6d5f7814d12f11dd83a7cc48bace6e705
ktunprasert <ktunprasert@outlook.com>
2025-07-03 23:59:24 +0100

    feat(main): download file

--- main.v
+++ main.v
@@ -3,6 +3,7 @@ module main
 import zztkm.vdotenv
 import flag
 import os
+import net.http
 
 @[xdoc: 'This is aoc-v, a tool to download Advent of Code data\nUsage: aoc-v <year> <day>']
 @[name: 'aoc-v']
@@ -11,6 +12,8 @@ mut:
 	show_help bool   @[long: help; short: h]
 	session   string @[long: session; short: s; xdoc: 'supply session manually, otherwise \$SESSION is read from env']
 }
+
+const aoc_url = 'https://adventofcode.com'
 
 fn main() {
 	vdotenv.load()
@@ -33,4 +36,22 @@ fn main() {
 		println(documentation)
 		exit(exit_code)
 	}
+
+	println('Downloading input for year ${args[0]} and day ${args[1]}...')
+	download(args[0], args[1], config.session) or {
+		println('Error downloading input: ${err}')
+		exit(-1)
+	}
+	println('Input downloaded successfully.')
+}
+
+fn download(year string, day string, session string) ! {
+	if !os.is_dir('./inputs') {
+		os.mkdir('./inputs')!
+	}
+
+	http.download_file_with_cookies('${aoc_url}/${year}/day/${day}/input', './inputs/${year}-${day}.txt',
+		{
+		'session': session
+	})!
 }
