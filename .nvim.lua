9e326976b7db8471e224054c8bc32c875fa7a02c
ktunprasert <ktunprasert@outlook.com>
2025-07-03 23:25:25 +0100

    batman

---
+++ .nvim.lua
@@ -0,0 +1,19 @@
+local term_ops = { auto_close = false }
+
+local entries = {
+    {
+        " v:run",
+        function() Snacks.terminal(("v run %s"):format(vim.fn.expand("%")), term_ops) end,
+        "v",
+    },
+    {
+        " v:test",
+        function() Snacks.terminal(("v -stats test %s"):format(vim.fn.expand("%:h")), term_ops) end,
+        "<space>",
+    },
+}
+
+
+for _, value in pairs(entries) do
+    Menus.append(value[1], value[2], value[3])
+end
---
+++ v.mod
@@ -0,0 +1,7 @@
+Module {
+	name: 'aoc_vlang'
+	description: 'aoc-vlang'
+	version: '0.0.0'
+	license: 'MIT'
+	dependencies: []
+}
