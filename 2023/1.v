e4be4b4698c196d6ff4274f61f2c34ca848308d6
ktunprasert <ktunprasert@outlook.com>
2025-07-04 13:25:28 +0100

    feat(2023.1): set the pair

--- 2023/1.v
+++ 2023/1.v
@@ -12,15 +12,19 @@ fn parse_input(lines []string) ![][]int {
 		mut tmp := []int{cap: 2}
 		for n in line {
 			if n.is_digit() {
+				if tmp.len == 2 {
+					tmp[1] = int(n - `0`)
+					continue
+				}
+
 				tmp << int(n - `0`)
 			}
 		}
 
 		out << match tmp.len {
-			0 { [0, 0] }
 			2 { tmp }
 			1 { tmp.repeat(2) }
-			else { [tmp.first(), tmp.last()] }
+			else { [0, 0] }
 		}
 	}
 
@@ -38,50 +42,60 @@ fn parse_input2(lines []string) ![][]int {
 		mut i := 0
 		for i < line.len {
 			n := line[i]
-			match true {
+			v := match true {
 				n >= `0` && n <= `9` {
-					tmp << int(n - `0`)
 					i++
+					int(n - `0`)
 				}
 				n == `o` && line.substr_unsafe(i, i + 3) == 'one' {
-					tmp << 1
 					i += 2
+					1
 				}
 				n == `t` && line.substr_unsafe(i, i + 3) == 'two' {
-					tmp << 2
 					i += 2
+					2
 				}
 				n == `t` && line.substr_unsafe(i, i + 5) == 'three' {
-					tmp << 3
 					i += 4
+					3
 				}
 				n == `f` && line.substr_unsafe(i, i + 4) == 'four' {
-					tmp << 4
 					i += 3
+					4
 				}
 				n == `f` && line.substr_unsafe(i, i + 4) == 'five' {
-					tmp << 5
 					i += 4
+					5
 				}
 				n == `s` && line.substr_unsafe(i, i + 3) == 'six' {
-					tmp << 6
 					i += 2
+					6
 				}
 				n == `s` && line.substr_unsafe(i, i + 5) == 'seven' {
-					tmp << 7
 					i += 4
+					7
 				}
 				n == `e` && line.substr_unsafe(i, i + 5) == 'eight' {
-					tmp << 8
 					i += 3
+					8
 				}
 				n == `n` && line.substr_unsafe(i, i + 4) == 'nine' {
-					tmp << 9
 					i += 2
+					9
 				}
 				else {
 					i++
+					-1
 				}
+			}
+
+			if v != -1 {
+				if tmp.len == 2 {
+					tmp[1] = v
+					continue
+				}
+
+				tmp << v
 			}
 		}
 
